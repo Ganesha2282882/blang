b808b2792a066a471124a1553afd114723ca7c69
char <char>
2021-03-04 16:39:31 -0500

    make it

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # blang
-A programming language.
+A programming language. Stands for "Brahma Language"
+
+It doesn't use a file, instead, it uses your shell.
+
+To build it, you neeed `pyinstaller`, and mark the `build` file executable.
+
+Then, execute like a shell script.
+# Help
+## Usage
+`APP` needs to be replaced as `./app` or `python app.py`.
+
+If you supply 0 arguments, an error will occur.
+### Print something to the screen
+You may only say 3 words.
+```
+APP say Hello World
+```
+will output:
+```
+Hello
+World
+```
+### Addition
+Do not use a `+`.
+```
+APP add 6 4
+```
+will output:
+```
+10
+```
+### Subtraction
+Same with Addition, but use `ta` instead of `add`.
+```
+APP ta 7 4
+```
+will output:
+```
+3
+```
+### Load file into RAM
+Just specify the file.
+```
+APP ram /dev/null
+```
+will output nothing.
+### try to get input forever
+```
+APP inp
+```
+will output:
+```
+% 
+```
+### `if ==`
+Similar to Python's `if ==`, but you can't specify what it will do. It will say `yes` or `no`.
+```
+APP if "hello" is "hello"
+```
+will output:
+```
+yes
+```
+```
+APP if "hello" is "world"
+```
+will output:
+```
+no
+```
+### `if !=`
+Just like `if is`, but use `if not` instead.
+### say `loop` forever
+```
+APP lop
+```
+will output:
+```
+loop
+loop
+loop
+[Snipped]
+```
--- app.py
+++ app.py
@@ -1,1 +1,75 @@
+import os
+import sys
 
+def nothing():
+    pass
+
+if sys.argv[1] == "say":
+    try:
+        print(sys.argv[2])
+
+    except:
+        nothing()
+
+    try:
+        print(sys.argv[3])
+
+    except:
+        nothing()
+
+    try:
+        print(sys.argv[4])
+
+    except:
+        nothing()
+
+elif sys.argv[1] == "add":
+    try:
+        print(int(sys.argv[2]) + int(sys.argv[3]))
+
+    except:
+        print("ERROR: Unknown hard error")
+
+elif sys.argv[1] == "ta":
+    try:
+        print(int(sys.argv[2]) - int(sys.argv[3]))
+
+    except:
+        print("ERROR: Unknown hard error")
+
+elif sys.argv[1] == "ram":
+    try:
+        open(sys.argv[2], "rb").close()
+
+    except:
+        print("ERROR: Unknown hard error")
+
+elif sys.argv[1] == "inp":
+    while True:
+        input("% ")
+
+elif sys.argv[1] == "if":
+    try:
+        if sys.argv[3] == "is":
+            if eval(sys.argv[2]) == eval(sys.argv[4]):
+               print("yes")
+
+            else:
+                print("no")
+
+        elif sys.argv[3] == "not":
+            if eval(sys.argv[2]) != eval(sys.argv[4]):
+                print("yes")
+
+            else:
+                print("no")
+
+    except:
+        print("ERROR: Unknown hard error")
+
+elif sys.argv[1] == "lop":
+    while True:
+        print("loop")
+
+else:
+    print("ERROR:", sys.argv[1], "is not a valid function.")
