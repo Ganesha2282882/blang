8c7043da04ad96dab48faaf2332340db422f0253
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-04-12 12:56:12 -0400

    Update bpkgman.py

--- bpkgman.py
+++ bpkgman.py
@@ -4,6 +4,9 @@ from sys import argv
 os.system("git clone https://github.com/Ganesha2282882/blang.git blangpkg-env")
 os.chdir("blangpkg-env")
 os.system("git checkout modpit")
+os.system("make getblang")
 
 for pkg in argv[1:]:
   os.system("patch < {}.diff".format(pkg))
+
+os.system("make")
