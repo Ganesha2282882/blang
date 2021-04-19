a2d5ca734dc2195428c41a9a3db309a96c1e8066
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-04-19 15:25:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def readprog(prog):
         
         elif line[0] == "scl":
             del line[0]
-            if line[0][1] == ":" and line[0][-1] == ":":
+            if line[0][0] == ":" and line[0][-1] == ":":
                 systemcmd = varlist[line[0][1]]
                 os.system(systemcmd)
                 
