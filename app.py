fe40e4bd32d4623622772b13e1651fa34fad99ea
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-04-19 16:22:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def readprog(prog):
             del line[0]
             if line[0][0] == ":" and line[0][-1] == ":":
                 for x in varlist[":{}:".format(line[0][1:-1])][0:]:
-                    print(x, end=" ")
+                    print(x, end="")
                  
                 print()
                 
