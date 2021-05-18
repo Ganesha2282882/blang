562360622a07c3d29ffbf6a675aa8eef3e727a84
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-05-18 10:05:56 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const joinNewsletter = () => {
+  let modalBox = document.getElementById("newsletterBox");
+  modalBox.open = ""
+}
+
+const unjoinNewsletter = () => {
+  let modalBox = document.getElementById("newsletterBox");
+  delete modalBox.open
+}
