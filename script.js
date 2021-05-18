39f6f3dd7e013f221214f919b65d9e3b5372a414
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-05-18 10:27:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const joinNewsletter = () => {
   alert("Want to join our weekly newsletter?\nUnfortunately, our tools can't send emails. You can, however, follow @blangCodeLang on Twitter.\n\nYou will also get other useful updates and even more!")
 }
 
-for (i = 0; i++; i < 1000) {
+let i = 0;
+for (i++; i < 1000) {
   console.log("This panel is not a blang console. This is a JavaScript console. JavaScript and blang are different.");
 }
