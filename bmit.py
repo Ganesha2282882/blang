96a5d11183c5092d14677790bfa33922b674011f
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-06-04 13:49:52 -0400

    Update bmit.py

--- bmit.py
+++ bmit.py
@@ -11,7 +11,7 @@ except FileExistsError:
   pass
 
 os.chdir("blang_modules")
-modfileobj = open("{}.blang".format(argw[0]), "w")
+modfileobj = open("{}.blang".format(argw[0]), "wb")
 modfileobj.write(reqs.get("https://raw.githubusercontent.com/blang-pl/blang-mod-repo/main/mods/{}.blang".format(argw[0])).content)
 modfileobj.close()
 os.chdir("..")
