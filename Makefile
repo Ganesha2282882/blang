340d9139ad5d623e3de78b011f3e8bc8b46a3ff3
Your Name <you@example.com>
2021-06-26 13:29:15 -0400

    add version manager

--- Makefile
+++ Makefile
@@ -7,9 +7,17 @@ blang:
 	./addshebang python bmit.py out/bmit
 	./addshebang python brepl.py out/brepl
 
+version_manager:
+	chmod +x addshebang
+	mkdir out 2> /dev/null || true
+	./addshebang python bvm.py out/bvm
+
+ivm: version_manager
+	cp out/bvm /usr/local/bin -fp
+
 install: blang
 	cp out/* /usr/local/bin -fp
 
-uninstall: blang
+uninstall: blang version_manager
 	chmod +x uninstall
 	./uninstall
---
+++ bvm.py
@@ -0,0 +1,27 @@
+from os.path import expanduser
+import requests
+from sys import argv
+import zipfile
+import os
+home = expanduser("~")
+os.chdir(home)
+argw = argv[1:]
+def nothing():
+    pass
+
+if argw[0] == "i":
+    try:
+        os.mkdir(".bvm")
+
+    except FileExistsError:
+        nothing()
+
+    print("Downloading blang version {}...".format(argw[1]))
+    open("blang.zip", "wb").write(requests.get("https://github.com/blang-pl/blang/archive/refs/tags/{}.zip".format(argw[1])).content)
+    print("Installing blang version {}...".format(argw[1]))
+    zipfile.ZipFile("blang.zip", "r").extractall(".bvm")
+    print("Installed. v/")
+
+elif argw[0] == "s":
+    cargs = " ".join(argv[3:])
+    os.system("python .bvm/blang-{}/app.py {}".format(argw[1], cargs))
